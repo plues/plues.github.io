004a569f89135895662605bbe9c32439c7728f5b
Travis <travis@travis-ci.org>
2017-06-08 18:50:37 +0000

    Update generated website sources. Build: 28

--- en/deploy.sh
+++ en/deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 # https://github.com/steveklabnik/automatically_update_github_pages_with_travis_example
+
+if [ "$TRAVIS_PULL_REQUEST" == "true" -o "$TRAVIS_BRANCH" != "sourceFiles" ]; then
+    echo "We only deploy changes from the branch 'sourceFiles'."
+    exit 0
+fi
+
 set -o errexit -o nounset
 
 cd _site
@@ -9,7 +15,7 @@ git init
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
-git remote add upstream "https://$GH_TOKEN@github.com/plues/plues.github.io.git"
+git remote add upstream "https://$GH_TOKEN@github.com/plues/plues.github.io.git" > /dev/null 2>&1
 git fetch upstream
 git reset upstream/master
 
@@ -18,4 +24,4 @@ touch .
 git add -A .
 git reset deploy.sh
 git commit -m "Update generated website sources. Build: $TRAVIS_BUILD_NUMBER"
-git push -q upstream HEAD:master
+git push -q upstream HEAD:master > /dev/null 2>&1
